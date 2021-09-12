2c58c29d5acc690e87e1717482033a0a241d53ea
Henry Jordan <henry.jordan@credera.com>
2021-09-12 15:46:53 -0500

    ADD | firebase + firestore configuration to access basic collecions

--- src/pages/category-page.tsx
+++ src/pages/category-page.tsx
@@ -1,10 +1,23 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
+import { db } from '../config/firebase';
+import { doc, getDoc } from "firebase/firestore";
 
 interface CategoryPageProps {
     category?: string,
 }
 
 export const CategoryPage: React.FunctionComponent<CategoryPageProps> = (props) => {
+
+    useEffect(() => {
+        const getCooks = async () => {
+            const docRef = doc(db, 'beef', '1')
+            const docSnap = await getDoc(docRef);
+            console.log(docSnap.data());
+        }
+
+        getCooks();
+    }, []);
+
     return (
         <div>HelloWorld</div>
     )
