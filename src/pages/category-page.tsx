e23dbbefa140580ee06913161b416fc620aedebc
Henry Jordan <henry.jordan@credera.com>
2021-09-12 15:08:44 -0500

    ADD | simple routing with react-router-dom

---
+++ src/pages/category-page.tsx
@@ -0,0 +1,11 @@
+import * as React from 'react';
+
+interface CategoryPageProps {
+    category?: string,
+}
+
+export const CategoryPage: React.FunctionComponent<CategoryPageProps> = (props) => {
+    return (
+        <div>HelloWorld</div>
+    )
+}
---
+++ src/routes.tsx
@@ -0,0 +1,33 @@
+import { Route, Switch, Redirect } from 'react-router-dom';
+import React from 'react';
+import map from 'lodash/map';
+import { CategoryPage } from './pages/category-page';
+
+const routeConfig = [
+    {
+        id: 'BEEF',
+        path: '/beef',
+        exact: true,
+        component: <CategoryPage />
+    },
+    {
+        id: 'DEFAULT',
+        path: '/*',
+        component: <Redirect to="/beef" />
+    }
+]
+
+export const Routes: React.FunctionComponent = () => {
+
+    const renderRoutes = () => {
+        const renderedRoutes = map(routeConfig, route => {
+            return <Route key={route.id} path={route.path} exact={route.exact} render={() => route.component}></Route>;
+        });
+
+        return renderedRoutes;
+    };
+
+    return <Switch>{renderRoutes()}</Switch>;
+};
+
+export default Routes;
